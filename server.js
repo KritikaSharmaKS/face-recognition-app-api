b1d7610fc5819684d5b945d87398a6771249f364
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-03-17 15:34:08 -0400

    profile linked to db

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const knex = require("knex");
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : '',
+      password : '',
+      database : 'smart-brain-db'
+    }
+});
+
+db.select('*').from('users').then(data => console.log(data));
 
 const app = express();
 app.use(bodyParser.json());
@@ -45,30 +58,32 @@ app.post('/signin', (req, res) => {
 //register
 app.post('/register', (req, res) => {
    const { email, password, name } = req.body;
-    database.users.push({
-        id: "125",
-        name: name,
-        email: email,
-        password: password,
-        entries: 0,
-        joined: new Date()
-    });
-    res.json(database.users[database.users.length-1]);
+   db('users')
+    .returning('*')
+    .insert({
+       name: name,
+       email: email,
+       joined: new Date()
+    })
+    .then(user => {
+        res.json(user[0]);
+    })
+    .catch(err => res.status(400).json("unable to register"))
 });
 
 //profile
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(element => {
-        if(element.id === id){
-            found = true;
-            return res.json(element);
-        }
-    });
-    if(!found){
-        res.status(404).json("user not found");
-    }
+    db.select('*').from('users').where({
+        id: id
+    })
+    .then(user => {
+        if(user.length){
+            res.json(user[0])
+        }else{
+            res.status(404).json("user not found");
+        }      
+    }).catch(err => res.status(400).json('Error Getting User'))
 });
 
 //image
